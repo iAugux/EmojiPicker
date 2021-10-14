dd12962dbb16b938c0714491546703a7e9802b9c
iAugus <iAugux@gmail.com>
2021-10-14 16:50:26 +0800

    delete pod in favor of spm

---
+++ EmojiPicker/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "EmojiPicker",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "EmojiPicker",
+            targets: ["EmojiPicker"]),
+    ],
+    targets: [
+        .target(
+            name: "EmojiPicker",
+            resources: [
+                .process("Resources/emojis.json"),
+            ]
+        ),
+    ]
+)
---
+++ EmojiPicker/Sources/EmojiPicker/Engine/Extension/String.swift
@@ -0,0 +1,34 @@
+//
+//  String.swift
+//  EmojiPicker
+//
+//  Created by levantAJ on 12/11/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        let language = UserDefaults.standard.string(forKey: Constant.CurrentLanguage.currentLanguageKey)
+        var bundle = Bundle.module
+        if Constant.CurrentLanguage.language != language {
+            if let path = bundle.path(forResource: language, ofType: "lproj") {
+                bundle = Bundle(path: path)!
+            }
+            Constant.CurrentLanguage.language = language
+            Constant.CurrentLanguage.bundle = bundle
+        } else {
+            bundle = Constant.CurrentLanguage.bundle
+        }
+        return NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
+    }
+}
+
+extension Constant {
+    enum CurrentLanguage {
+        static var language: String?
+        static var bundle = Bundle.module
+        static let currentLanguageKey = "com.levantAJ.EmojiPicker.currentLanguage"
+    }
+}
